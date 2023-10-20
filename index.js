3ca0a61a12af362ec25c09d8353c261b870c4f80
Your Name <you@example.com>
2023-10-20 06:32:26 +0600

    updated index.js about mongodb

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,28 +12,6 @@ app.use(cors())
 
 // user name: dragonNews
 // password: oNFitiVl7LV4eKR8
-
-const uri = "mongodb+srv://dragonNews:oNFitiVl7LV4eKR8@cluster0.cvt2jva.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
